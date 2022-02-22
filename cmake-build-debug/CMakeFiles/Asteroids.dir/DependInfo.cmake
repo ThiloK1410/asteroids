12c26a62cb2cf7864b74f7e097f36c54be9b628c
ThiloK1410 <58908397+ThiloK1410@users.noreply.github.com>
2022-02-22 17:05:36 +0100

    Add files via upload
    SFML template upload

---
+++ cmake-build-debug/CMakeFiles/Asteroids.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/thilo/CLionProjects/Asteroids/src/Engine.cpp" "C:/Users/thilo/CLionProjects/Asteroids/cmake-build-debug/CMakeFiles/Asteroids.dir/src/Engine.cpp.obj"
+  "C:/Users/thilo/CLionProjects/Asteroids/src/main.cpp" "C:/Users/thilo/CLionProjects/Asteroids/cmake-build-debug/CMakeFiles/Asteroids.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../headers"
+  "c:/SFML/include"
+  "/SFML/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Asteroids.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Asteroids.exe"
+  "Asteroids.exe.manifest"
+  "Asteroids.pdb"
+  "CMakeFiles/Asteroids.dir/src/Engine.cpp.obj"
+  "CMakeFiles/Asteroids.dir/src/main.cpp.obj"
+  "libAsteroids.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Asteroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
